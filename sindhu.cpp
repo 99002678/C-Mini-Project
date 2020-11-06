fafa92719e71793983042574a53e5331adbd665f
99002436 <71481120+O-Deepak@users.noreply.github.com>
2020-11-06 13:33:58 +0530

    Update sindhu.cpp

--- sindhu.cpp
+++ sindhu.cpp
@@ -145,3 +145,212 @@ void medicineType::takeOrder()
 	system("cls");
 }
 }
+
+void medicineType::modify()	{
+ system("cls");
+ int i, ch, sid;
+ bool found;
+ found = false;
+ temp = startPtr;
+ std::cout<<"Enter Receipt Number To Modify: ";
+ std::cin>>sid;
+ if (temp==NULL && sid==0)
+ {
+    std::cout<<"SORRY!!!!!!!! NO RECORD TO MODIFY..!"<<endl;
+ }
+
+ else{
+ 	while(temp !=NULL && !found)
+	{
+		if (temp->recieptNumber==sid)
+		{
+			found = true;
+		}
+		else
+		{
+			temp = temp -> next;
+		}
+    if (found)
+    {
+	std::cout << "Change  Order Number: ";
+    std::cin >> temp->recieptNumber;
+	std::cout<< "Change Customer Name: ";
+	std::cin>> temp->customerName;
+	std::cout<<"Change Date : ";
+	std::cin>>temp->date;
+	std::cout << "How many New Medicine would you like to Change:"<< endl;
+	std::cout<<"( Maximum is 10 order for each transaction ) \n";
+	std::cout << "  " ;
+	std::cin >> temp->x;
+	if (temp->x >10)
+	{
+		std::cout << "Your order is exceeding the maximum count of order !";
+		system("pause");
+	}
+	else{
+	for (i=0; i<temp->x; i++)
+	{
+		
+		std::cout << "Please enter your selection to modify: "<<endl;
+		std::cin>> temp->menu2[i];
+        std::cout<< "Change Medicine Name: " <<temp->medicineName[temp->menu2[i]-1]<<endl;
+        std::cout << "How many New medicine do you want: ";
+        std::cin >> temp->quantity[i];
+        temp->amount[i] = temp->quantity[i] * temp->Medicine[temp->menu2[i]-1];
+        std::cout << "The amount You need to pay After Modify  is: " << temp->amount[i]<<"Rs "<<endl;
+        system("PAUSE");
+	}
+	temp = temp->next;
+	system("cls");
+	
+	}
+
+ std::cout<<"RECORD MODIFIED....!"<<endl;
+ }
+ else 
+ {
+ 	if(temp != NULL && temp->recieptNumber != sid)
+ 	{
+ 	std::cout<<"Invalid Reciept Number...!"<<endl;
+    }
+ }
+}
+}
+}		
+
+void medicineType::dailySummary()	{
+	int i,num;
+	system("cls");
+	node *temp ;
+
+	temp=startPtr;
+
+
+	if(temp == NULL) 
+	{
+		std::cout << "\t\t\tThere is no Order to show\n\t\t\tSo The List is Empty\n\n\n";
+	}
+	else{
+		std::cout<<"\n";
+		std::cout<<"==========================================================================="<<endl;
+		std::cout <<" \t\tHere is the Daily Summary of All Orders \n"; 
+		std::cout<<"==========================================================================="<<endl;
+
+		while(temp!=NULL)
+		{
+		
+				std::cout <<"Reciept Number : "<<temp->recieptNumber;
+				std::cout <<"\n";
+				std::cout<<"Customer Name: "<<temp->customerName<<endl;
+				
+				std::cout<<"Order Date : "<<temp->date<<endl;
+				
+				std::cout<<"____________________________________________________________________________"<<endl;
+			
+				std::cout << "==========================================================================" << endl;
+				std::cout << "|  Medicine Type |   Medicine Name    |    Quantity     |    Total Price |" << endl;
+				std::cout << "=======++==================++================++===============++==========" << endl;
+			for (i=0;i<temp->x;i++)
+			{
+				std::cout << temp->type <<"  \t\t";
+				std::cout<<temp->medicineName[temp->menu2[i]-1]<<"\t\t";
+				std::cout<<temp->quantity[i] <<"\t\t";
+				std::cout<< temp->amount[i]<<"Rs "<<endl;
+				std::cout<<"_____________________________________________________________________________"<<endl;
+			}
+			
+			temp->total = temp->amount[0]+temp->amount[1]+temp->amount[2]+temp->amount[3]+temp->amount[4]+temp->amount[5]+temp->amount[6]+temp->amount[7]
+			+temp->amount[8]+temp->amount[9];
+			std::cout<<"Total Bill is : "<<temp->total;
+		
+			std::cout <<"\n";
+			std::cout <<"\n";
+			std::cout <<"\n_______________________________________________________________________________\n";
+				
+			temp=temp->next;
+		}
+	}
+}		
+void medicineType::exit() 
+{
+	std::cout<<"\nYou choose to exit.\n"<<endl;
+}
+
+int main()
+{
+
+	system("COLOR 79");		
+	medicineType medicine;
+	int menu;
+	do
+	{
+		system("cls");
+		std::cout<<"\t\t\tPharmacy Management System - UNIMAS\n";
+		std::cout<<"\t\t**************************************************\n\n";
+		std::cout<<"\t\t**************************************************\n";
+		std::cout<<"\t\t||\t1. Take new Medicine order \t\t ||\n";
+		std::cout<<"\t\t||\t2. delete latest Medicine order\t\t ||\n";
+		std::cout<<"\t\t||\t3. Modify Order List \t\t\t ||\n";
+		std::cout<<"\t\t||\t4. Print the Reciept and Make Payment \t ||\n";
+		std::cout<<"\t\t||\t5. Daily Summary of total Sale \t\t ||\n";
+		std::cout<<"\t\t||\t6. Exit\t\t\t\t\t ||\n";
+		std::cout<<"\t\t**************************************************\n";
+		std::cout<<"Enter choice: ";
+
+		std::cin>>menu;
+
+		switch (menu)
+		{
+		case 1:
+			{
+				medicine.takeOrder();	
+				break;
+			}	
+		
+		
+		case 2:
+			{
+				medicine.deleteOrder();	
+				system("PAUSE");
+				break;
+			}	
+			
+		case 3:
+			{
+				medicine.modify();
+				system("PAUSE");
+				break;
+			}	
+
+		case 4:
+			{
+				medicine.orderList();	
+				system("PAUSE");
+				break;
+			}	
+		case 5:
+			{
+				medicine.dailySummary();	
+				system("PAUSE");
+				break;
+			}	
+        case 6:
+			{
+				medicine.exit();	
+				goto a;
+				break;
+			}	
+
+		
+		default:
+			{
+				std::cout<<"You enter invalid input\nre-enter the input\n"<<endl;
+				break;
+			}
+		}
+	}while(menu!=6);
+	a://goto
+	std::cout<<"thank you"<<endl;
+	system ("PAUSE");
+	return 0;
+}
